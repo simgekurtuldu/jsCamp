c85ce169af0090099be61cf413c9d81a216c85bf
simge kurtuldu <simgekurtuldu@email.com>
2021-10-25 13:15:07 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba Kodlama.io")
+// JS type safe değildir.
+let dolorDun = 9.20
+let dolarBugun=9.30
+{
+    let dolarBugun=9.10
+}
+console.log(dolarBugun)
+
+const euroDun =11.2
+//euroDun = 11
+console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi" , "Emlak Konut Kredisi","Kamu Konut Kredisi"]
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+    
+}
+//console.log(konutKredileri)
